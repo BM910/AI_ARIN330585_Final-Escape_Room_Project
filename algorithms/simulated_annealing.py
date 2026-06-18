882e56da628c748b00e6f3abf946bfdad64e333e
Phạm Trần Đức Lương <luongpham.060106@gmail.com>
2026-06-18 14:59:26 +0700

    Update algorithms

--- algorithms/simulated_annealing.py
+++ algorithms/simulated_annealing.py
@@ -1,4 +1,4 @@
-import copy
+import copy, random, math
 
 class State:
     def __init__(self, map, x, y, energy, keys):
@@ -58,7 +58,7 @@ class State:
         for row in range(len(self.map)):
             for col in range(len(self.map[0])):
                 if self.map[row][col] == 'E':
-                    return abs(row - self.x) + abs(col - self.y)
+                    return abs(row - self.x) + abs(col - self.y) + len(self.keys) * 2
         
         return None
         
@@ -105,63 +105,34 @@ def get_result_path(node: Node):
     path.reverse()
     return path
 
-# Lấy ra node có f(n) nhỏ nhất từ frontier
-def pop_min_cost_state(frontier):
-    lowest_cost_node = min(frontier.values(), key=lambda node: node.cost['f_n'])
 
-    del frontier[lowest_cost_node.state.get_tuple_representation()]
+def simulated_annealing(initial_state):
+    current_node = Node(initial_state, None, None, initial_state.calculate_heuristic())
 
-    return lowest_cost_node
+    T = 100      # Gán cố định
+    T_min = 0.01
+    alpha = 0.99
 
-
-def a_star(initial_state):
-    if not initial_state: #Nếu initial_state là None thì không làm gì hết
-        return None
-    
-    # Khởi tạo Node
-    heuristic_cost = initial_state.calculate_heuristic()
-    node = Node(initial_state, parent=None, action=None, cost={'g_n': 0, 'h_n': heuristic_cost, 'f_n': heuristic_cost})
-
-    frontier = {node.state.get_tuple_representation(): node}    # Cài đặt frontier dạng dict, key = state dạng tuple --> Tìm kiếm dễ dàng
-
-    reached = dict()    # Lưu state dạng tuple và g(n)
-    while frontier:
-        node = pop_min_cost_state(frontier)
-
-        if node.state.is_at_goal():
-            return get_result_path(node)
-
-        reached[node.state.get_tuple_representation()] = node.cost['g_n']      # Lưu biểu diễn dạng tuple kèm g(node)
+    while T > T_min: 
+        if current_node.state.is_at_goal():
+            return get_result_path(current_node)
         
-        for action in node.state.get_moves():
-            next_state = generate_new_state(node.state, action)
-            if next_state is None:
-                continue
-            
-            tuple_state = next_state.get_tuple_representation()
-            g_new = node.cost['g_n'] + ((node.state.energy - next_state.energy) if  node.state.energy > next_state.energy else 0)
+        moves = current_node.state.get_moves()
+        if not moves:
+            return None
+        
+        move = random.choice(moves)
+        next_state = generate_new_state(current_node.state, move)
+        h_cost_next_state = next_state.calculate_heuristic()
 
-            if tuple_state in reached:     # Nếu state đã xét và có cost tối ưu hơn --> Xóa trong reached để xét lại
-                if g_new >= reached[next_state.get_tuple_representation()]:
-                    continue
-                else:
-                    del reached[next_state.get_tuple_representation()]
-            
-            if tuple_state in frontier:     # Nếu state trong frontier và có cost tối ưu hơn --> Thay thế node trong frontier
-                node_in_frontier = frontier[tuple_state]
-                if node_in_frontier.cost['g_n'] > g_new:
-                    node_in_frontier.cost['g_n'] = g_new
-                    node_in_frontier.cost['f_n'] = g_new + node_in_frontier.cost['h_n']
-                    node_in_frontier.parent = node
-
-                continue
-
-            if not tuple_state in frontier and not tuple_state in reached:
-
-                child_node = Node(next_state, node, action, None)
-                heuristic_cost = next_state.calculate_heuristic()
-                child_node.cost = {'g_n': g_new, 'h_n': heuristic_cost, 'f_n': g_new + heuristic_cost}
-
-                frontier[tuple_state] = child_node
+        delta = h_cost_next_state - current_node.cost
+        if delta < 0:
+            current_node = Node(next_state, current_node, move, h_cost_next_state)
+        else:
+            p = math.exp(-delta/T)
+            if random.uniform(0, 1) < p:
+                current_node = Node(next_state, current_node, move, h_cost_next_state)
+        
+        T *= alpha
 
     return None
